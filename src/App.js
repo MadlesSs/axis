5681588114f5dccd86c1c532cfda4296d6d99098
Radovan Littva <radovan.littva@gmail.com>
2021-01-07 21:30:33 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import { MyDatePicker } from './components/DatePicker';
 
 function App() {
+  const [startDate, setStartDate] = React.useState(null);
+  const oneDay = 24 * 60 * 60 * 1000;
   return (
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>When did you give your wind to AXIS?</h1>
+        <MyDatePicker sendDateToParent={date => setStartDate(date)} />
+        <p>{startDate ? 'I\'ts been neverending ' + Math.round(Math.abs((startDate - new Date()) / oneDay)) + ' days' : null}</p>
       </header>
     </div>
   );
